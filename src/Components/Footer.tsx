d931a311475b0466be91d845d7bb958de33956f3
SiriChandhana06 <sirisiri3006@gmail.com>
2024-10-20 21:03:51 +0530

    frontend host

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import logo from '../Assests/logo.png';
-import Modal from './Model';
+// import Modal from './Model';
 
 const Footer = () => {
   return (
